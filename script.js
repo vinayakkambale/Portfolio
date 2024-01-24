dc9429b93fd2c6e30fd495f60b6ce0be55987f96
Manish Chavan <manish.chavan113@gmail.com>
2024-01-24 13:01:29 +0530

    fixed syntax issue

--- script.js
+++ script.js
@@ -6,4 +6,4 @@ const toggleNav = () => {
  nav_header.classList.toggle("active");
 }
 
-mobile_nav.addEventListener("click", () => toggleNav());q
+mobile_nav.addEventListener("click", () => toggleNav());
